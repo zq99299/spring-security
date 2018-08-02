a785d848796aea645d154825903a8cc7d2c8787d
zhuqiang <99299684@qq.com>
2018-08-03 00:03:27 +0800

    项目模块改变；启用security-browser

---
+++ security-browser/src/main/java/cn/mrcode/imooc/springsecurity/securitybrowser/SecurityBrowserApplication.java
@@ -0,0 +1,12 @@
+package cn.mrcode.imooc.springsecurity.securitybrowser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityBrowserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityBrowserApplication.class, args);
+	}
+}
