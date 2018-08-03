33e59b23e4560ac877d25e25c80799faa80711ae
zhuqiang <zhuqiang@tidebuy.net>
2018-08-03 13:19:40 +0800

    解决打包，子包无法打包的问题、子包注解无法扫描的问题

--- security-browser/src/main/java/cn/mrcode/imooc/springsecurity/securitybrowser/SecurityBrowserApplication.java
+++ security-browser/src/main/java/cn/mrcode/imooc/springsecurity/securitybrowser/SecurityBrowserApplication.java
@@ -1,6 +1,5 @@
 package cn.mrcode.imooc.springsecurity.securitybrowser;
 
-import cn.mrcode.imooc.springsecurity.securitycore.SecurityCoreApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,9 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SecurityBrowserApplication {
 
     public static void main(String[] args) {
-        Class[] applications = new Class[2];
-        applications[0] = SecurityCoreApplication.class;
-        applications[1] = SecurityBrowserApplication.class;
-        SpringApplication.run(applications, args);
+        SpringApplication.run(SecurityBrowserApplication.class, args);
     }
 }
--- security-demo/src/main/java/com/example/demo/DemoApplication.java
+++ security-demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,22 +1,22 @@
 package com.example.demo;
 
-import cn.mrcode.imooc.springsecurity.securitybrowser.SecurityBrowserApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages={"com.example.demo","cn.mrcode.imooc.springsecurity.securitybrowser"})
 @RestController
 @EnableSwagger2
 public class DemoApplication {
 
     public static void main(String[] args) {
-        Class[] applications = new Class[2];
-        applications[0] = SecurityBrowserApplication.class;
-        applications[1] = DemoApplication.class;
-        SpringApplication.run(applications, args);
+//        Class[] applications = new Class[2];
+//        applications[0] = SecurityBrowserApplication.class;
+//        applications[1] = DemoApplication.class;
+//        SpringApplication.run(applications, args);
+        SpringApplication.run(DemoApplication.class, args);
     }
 
     @GetMapping("/hello")
