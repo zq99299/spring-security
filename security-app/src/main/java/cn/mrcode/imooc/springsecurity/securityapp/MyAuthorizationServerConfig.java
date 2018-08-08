97af10f8fb3e7b765ad5a65340ac0322410eab7d
zhuqiang <zhuqiang@tidebuy.net>
2018-08-08 13:57:20 +0800

    update

--- security-app/src/main/java/cn/mrcode/imooc/springsecurity/securityapp/MyAuthorizationServerConfig.java
+++ security-app/src/main/java/cn/mrcode/imooc/springsecurity/securityapp/MyAuthorizationServerConfig.java
@@ -1,4 +1,4 @@
-package cn.mrcode.imooc.springsecurity.securitycore;
+package cn.mrcode.imooc.springsecurity.securityapp;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
